0e7ab75fbeec128f6123d6e3c375a250df22dc40
Lorenzo <lrenouvin98@gmail.com>
2021-03-17 15:05:27 +0100

    SERVER

--- Chatbot_python/bot.py
+++ Chatbot_python/bot.py
@@ -9,12 +9,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents = intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for server in client.guilds:
+        if server.name == SERVER:
+            break
+    print(f'{client.user} is connected to the following server:\n'
+          f'{server.name}(id: {server.id})')
+    members = '\n - '.join([member.name for member in server.members])
+    print(f'Server\'s Members:\n - {members}')
 
 client.run(TOKEN)
 
